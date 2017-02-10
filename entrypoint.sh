0a49ed7d1e59fcaa9dc9c3a51cabb804767328f1
endokai <my1130546419@gmail.com>
2017-02-11 01:49:21 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
+if [ ! -d /var/log/vpnserver ]; then
+    mkdir -p /var/log/vpnserver
+fi
+
+if [ ! -d /opt/vpnserver/packet_log ]; then
+    ln -s /var/log/vpnserver /opt/vpnserver/packet_log
+fi
+
+if [ ! -d /opt/vpnserver/security_log ]; then
+    ln -s /var/log/vpnserver /opt/vpnserver/security_log
+fi
+
+if [ ! -d /opt/vpnserver/server_log ]; then
+    ln -s /var/log/vpnserver /opt/vpnserver/server_log
+fi
+
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
